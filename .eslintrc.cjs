24fa8d5841e8aa397afac329e0309f3fec29a322
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-07-28 19:59:17 +0530

    update setting for linting and formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,71 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname, // Ensure the path to your tsconfig.json is correct
   },
-  "plugins": [
-    "@typescript-eslint"
+  plugins: [
+    '@typescript-eslint',
+    'simple-import-sort',
+    'import',
+    'prettier',
+    'jsx-a11y',
+    'react-hooks',
   ],
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:jsx-a11y/recommended', // Accessibility
+    'plugin:react-hooks/recommended', // React hooks
+    'prettier', // Make sure to put it last in the extends array
   ],
-  "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
       {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "argsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: '^_',
+      },
     ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
+    '@typescript-eslint/require-await': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
       {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'error',
+    'prettier/prettier': 'warn',
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Ensure the eslint-plugin-react can automatically detect the version of React to use
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // TypeScript specific rules can go here
+      },
+    },
+  ],
+};
 module.exports = config;
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,15 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  endOfLine: 'auto',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5', // Adds trailing commas where valid in ES5 (objects, arrays, etc.)
+  singleAttributePerLine: false,
+  jsxSingleQuote: true, // Use single quotes in JSX
+  tailwindFunctions: ['cn'],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
