3d0b163377b764d75ae6668f0129ed389c516d57
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-09-01 01:07:55 +0530

    fix: hydration error for `react-theme` className on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
       <body>
         <AppProviders>{children}</AppProviders>
       </body>
